676ad2322066913fd7e156a5043c62de38098944
dalipbs <dalipbs@gmail.com>
2017-04-05 15:47:17 +0530

    update  credetial
    update  credetial

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: '605722eb-0060-4031-98cf-b93573006b24',
+    appPassword: '5pBAmGgLUPaaRfbLpfNA0om'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
