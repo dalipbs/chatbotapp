bd92c57420ced85250a1c58260249494f23a4a1c
dalipbs <dalipbs@gmail.com>
2017-04-10 14:39:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var zipcode ={
+    zipid:0000
+};
+
+var personaldetails ={
+    mobile:000000000,
+    name:null
+};
+
 //var intents = new builder.IntentDialog();
 //=========================================================
 // Bot Setup
@@ -12,8 +21,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: '605722eb-0060-4031-98cf-b93573006b24',
-    appPassword: '5pBAmGgLUPaaRfbLpfNA0om'
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
@@ -27,9 +36,63 @@ var recognizer = new builder.LuisRecognizer(model);
 //bot.recognizer(recognizer);
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
 bot.dialog('/', dialog);
-dialog.matches('Hello', builder.DialogAction.send('Hello sir'));
-dialog.matches('AgentInfo', builder.DialogAction.send('Fetching Agent Info'));
-dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
+dialog.matches('Hello', builder.DialogAction.send('Hello there'));
+//dialog.matches('AgentInfo', builder.DialogAction.send('Fetching Agent Info'));
+dialog.matches('AgentInfo',[
+    function (session) {
+       
+               builder.Prompts.text(session, 'Please enter the 10 digit  mobile no for verifaction');
+           
+            
+    },
+    function (session, results,next) {
+             personaldetails.mobile =results.response;
+             if(personaldetails.mobile!=1234567890)
+            {
+                session.send('Information not matching with the system');
+            }
+
+           else
+           {
+               personaldetails.name='Dalip';
+             session.send('Hello '+personaldetails.name);
+               next();
+           }},
+
+function (session) {
+               
+
+            builder.Prompts.text(session, 'Please enter the zipcode');
+            zipcode.zipid=results.response;
+            session.send('Fetching Agent Info for zip '+zipcode.zipid);
+    },
+
+function (session,results) {
+               
+            zipcode.zipid=results.response;
+            session.send('Fetching Agent Info for zip '+zipcode.zipid);
+    }
+
+
+    
+]);
+
+// bot.dialog('/profile', [
+//     function (session) {
+//         builder.Prompts.text(session, 'Hi! What is your name?');
+//     },
+//     function (session, results) {
+//         session.userData.name = results.response;
+//         session.endDialog();
+//     }
+// ]);
+
+
+
+
+
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand."));
+//var zipdata = {};
 /*bot.dialog('/', intents);
 
 intents.matches(/^change name/i, [
